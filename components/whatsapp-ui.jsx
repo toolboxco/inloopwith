a39062d1199a02d4ec3e431071054c75d3688c92
therexone <renerjake@gmail.com>
2020-11-17 02:46:01 +0530

    feat: wa header

--- components/whatsapp-ui.jsx
+++ components/whatsapp-ui.jsx
@@ -2,6 +2,8 @@ import styles from '../styles/whatsappUI.module.css';
 import NetworkIcon from '../assets/network.svg';
 import WifiIcon from '../assets/wifi.svg';
 import BatteryIcon from '../assets/battery.svg';
+import BackIcon from '../assets/back-arrow.svg';
+import PhoneIcon from '../assets/phone.svg';
 
 const WhatsappUI = () => {
     return (
@@ -13,6 +15,15 @@ const WhatsappUI = () => {
                     <WifiIcon />
                     <BatteryIcon />
                 </div>
+            </div>
+            <div className={styles.waHeader}>
+                <BackIcon />
+                <img src="/static/img/logo.png" alt="inloopwith" />
+                <div>
+                    <h2>In loop with Tech</h2>
+                    <sub>+ 1 (416) 877-453, +1 (424) 543-939...</sub>
+                </div>
+                <PhoneIcon/>
             </div>
         </div>
     );
