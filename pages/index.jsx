c997fbebf32a3c1f822c5576bc9b7d4863302180
therexone <renerjake@gmail.com>
2020-11-19 00:29:05 +0530

    fix: section order

--- pages/index.jsx
+++ pages/index.jsx
@@ -20,8 +20,8 @@ export default function Home() {
             </Head>
 
             <main className={styles.main}>
-                <DemoSection />
                 <InfoSection />
+                <DemoSection />
             </main>
         </>
     );
