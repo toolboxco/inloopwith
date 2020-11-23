5889ac998bb7fdeb3cbf7931bab6ffc6fca69255
therexone <renerjake@gmail.com>
2020-11-23 17:45:12 +0530

    feat: existing content ui revamp

---
+++ components/heading.jsx
@@ -0,0 +1,3 @@
+import styles from '../styles/heading.module.scss';
+
+const Heading = () => <div className={styles.heading}></div>;
--- pages/index.jsx
+++ pages/index.jsx
@@ -10,7 +10,7 @@ export default function Home() {
                 <title>In loop with</title>
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
                 <link
-                    href="https://fonts.googleapis.com/css2?family=Anonymous+Pro:wght@400;700&display=swap"
+                    href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap"
                     rel="stylesheet"
                 />
                 <link
