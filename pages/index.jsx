2282a9af9fb59e13d321edf3470e97b08feb4d02
Aakash Goel <aakash.lpin@gmail.com>
2020-11-28 13:35:26 +0530

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,7 +11,6 @@ export default function Home() {
     return (
         <>
             <Head>
-                <title>In loop</title>
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
                 <link
                     href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap"
