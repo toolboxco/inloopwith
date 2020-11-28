d932e5beeebabae75035769efd96bbc478ac243b
Aakash Goel <aakash.lpin@gmail.com>
2020-11-28 17:44:14 +0530

    fix: image cdn

--- components/Testimonials/testimonial-card.jsx
+++ components/Testimonials/testimonial-card.jsx
@@ -1,8 +1,15 @@
 import styles from '../../styles/testimonials.module.scss';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const TestimonialCard = ({ name, designation, testimonial, img }) => (
     <div className={styles.card}>
-        <img src={`/static/img/testimonials/${img}.png`} alt={name} />
+        <img
+            src={`${
+                isProd ? 'https://toolboxco.imgix.net' : ''
+            }/static/img/testimonials/${img}.png${isProd ? '?w=80' : ''}`}
+            alt={name}
+        />
         <h3>{name}</h3>
         <sub>{designation}</sub>
         <i>"{testimonial}"</i>
